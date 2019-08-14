98b2bd85e2541def4471a34c4b7f42bfa7c676f1
Michael Lynn <merlynn@gmail.com>
2019-08-14 16:31:21 -0400

    Initial Commit - Part 2

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -46,5 +46,19 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK - Add New Items
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("Success!")
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
